2871f5ed285550ff7b8a5d0a319e394a2c7c53b0
neilalden <escobin.neil@gmail.com>
2020-08-15 01:50:06 +0800

    Updates

---
+++ precache-manifest.e85ccc4b6d774998ac17581ab438daaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b652061e9f7f8d14af338217654c6066",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "725056f43b4f441b70d3",
+    "url": "/react_todo_list/static/css/main.4cbf8487.chunk.css"
+  },
+  {
+    "revision": "f3b33a566712a8656ce0",
+    "url": "/react_todo_list/static/js/2.b798610f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_list/static/js/2.b798610f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725056f43b4f441b70d3",
+    "url": "/react_todo_list/static/js/main.903c236b.chunk.js"
+  },
+  {
+    "revision": "8cd4d5f64c0f2a89cf49",
+    "url": "/react_todo_list/static/js/runtime-main.121b684b.js"
+  }
+]);
